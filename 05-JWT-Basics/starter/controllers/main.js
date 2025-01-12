5d90150fe4b26a1423194ed992f7b447482fdef1
IrynaSt <irinastoetski13@gmail.com>
2025-01-12 11:28:29 -0600

    work with the video

---
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -0,0 +1,30 @@
+const jsonwebtoken = require("jsonwebtoken");
+const { BadRequestError } = require("../errors");
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  console.log(username, password);
+
+  if (!username || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+  const id = new Date().getDate();
+
+  const token = jsonwebtoken.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(200).json({ msg: "user created", token });
+};
+
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello ${req.user.username}, here is your lucky number: ${luckyNumber}`,
+  });
+};
+
+module.exports = {
+  login,
+  dashboard,
+};
